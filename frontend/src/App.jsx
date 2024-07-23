d5133daeeedd4d64be83f8414407fc4602ad22cc
Nirmith Victor D'Almeida <63338983+NirmithDev@users.noreply.github.com>
2024-07-23 16:51:55 -0400

    Add Pages and basic formatting templates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigation } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
 
