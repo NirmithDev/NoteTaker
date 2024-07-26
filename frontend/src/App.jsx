99d36930301d54f10fcc96a64aa866f4426f2aef
Nirmith Victor D'Almeida <63338983+NirmithDev@users.noreply.github.com>
2024-07-26 16:11:54 -0400

    Add routing and protection for routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
@@ -7,12 +7,28 @@ import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
 
+function Logout(){
+  localStorage.clear();
+  return <Navigate to='/Login'></Navigate>
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/register' element={<RegisterAndLogout/>}></Route>
+      <Route path='/Contact' element={<Contact/>}></Route>
+      <Route path='*' element={<NotFound/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
