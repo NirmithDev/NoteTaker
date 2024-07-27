b9c8833c6d61bf509f0cabcc3a758909290ad36e
Nirmith Victor D'Almeida <63338983+NirmithDev@users.noreply.github.com>
2024-07-27 18:15:01 -0400

    add Note Creation input fields and handling
    This will need a rework and improvement

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import api from '../api'
 
 const Home = () => {
+  //all notes
+  const [notes,setNotes]= useState([]);
+  const [content,setContent] = useState("");
+  const [title,setTitle] = useState("")
+
+  useEffect(()=>{
+    getNotes();
+  },[])
+  const getNotes = () => {
+    api.get("/api/notes/").then((res)=>res.data).then((data)=>{setNotes(data); console.log(data)}).catch((error)=>alert(error))
+  }
+  //delete notes
+  const deleteNote = (id) =>{
+    api.delete(`/api/notes/delete/${id}/`).then((res)=>{
+      if(res.status === 204) alert("Note Deleted")
+      else alert("Note Failed to delete")
+      getNotes()
+    }).catch((error)=>alert(error))
+  }
+  //create notes
+  const createNote =(e)=>{
+    e.preventDefault()
+    api.post("/api/notes/",{content,title}).then((res)=>{
+      if(res.status === 201) alert("Note created successfully")
+      else alert("Failed to make note")
+      getNotes()
+    }).catch((error)=>alert(error))
+  }
   return (
     <div>
-      Welcome to Note App
+      <div>
+        <h2>Notes</h2>
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor='title'>Title:</label>
+        <br></br>
+        <input type='text' id="title" name='title' required onChange={(e)=>setTitle(e.target.value)}value={title}></input>
+        <label htmlFor='content'>Content:</label>
+        <br></br>
+        <textarea id="content" name='content' required onChange={(e)=>setContent(e.target.value)}value={content}></textarea>
+        <br></br>
+        <input type='submit' value="Submit"></input>
+      </form>
     </div>
   )
 }
