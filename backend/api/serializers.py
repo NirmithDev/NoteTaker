a2512d0b0669963c1a0a49284615bdaf2c912562
Nirmith Victor D'Almeida <63338983+NirmithDev@users.noreply.github.com>
2024-07-30 14:12:46 -0400

    Add backend support for user profile setup

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,22 +1,55 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Note
-#django uses ORM (Object relational mapping) -> handles all database operations
-# we will handle json in here
+from .models import Note, Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'location', 'birth_date']
+
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
     class Meta:
         model = User
-        fields = ["id","username","password"]
-        # only write when password is created we do not want to return it
-        extra_kwargs = {"password":{"write_only":True}}
-    #method to create a new version of our user
+        fields = ["id", "username", "password", "profile"]
+        extra_kwargs = {"password": {"write_only": True}}
+
     def create(self, validated_data):
-        user=User.objects.create_user(**validated_data)
+        profile_data = validated_data.pop('profile', {})
+        user = User.objects.create_user(**validated_data)
+        
+        # Check if the profile already exists
+        if not hasattr(user, 'profile'):
+            Profile.objects.create(user=user, **profile_data)
+        else:
+            # Update existing profile
+            profile = user.profile
+            profile.bio = profile_data.get('bio', profile.bio)
+            profile.location = profile_data.get('location', profile.location)
+            profile.birth_date = profile_data.get('birth_date', profile.birth_date)
+            profile.save()
+
         return user
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', {})
+        profile = instance.profile
+
+        instance.username = validated_data.get('username', instance.username)
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+        instance.save()
+
+        profile.bio = profile_data.get('bio', profile.bio)
+        profile.location = profile_data.get('location', profile.location)
+        profile.birth_date = profile_data.get('birth_date', profile.birth_date)
+        profile.save()
+
+        return instance
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ["id","title","content","created_at","author"]
-        extra_kwargs = {"author":{"read_only":True}}
-    
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
